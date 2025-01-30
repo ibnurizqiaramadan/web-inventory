0f113d934593fa352612d55fb714583a261b5fac
Ibnurizqiaramadan <32134721+ibnurizqiaramadan@users.noreply.github.com>
2025-01-30 15:42:57 +0000

    Change connection ddetails

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php 
-$koneksi = mysqli_connect("localhost", "root", "", "inventory");
+$koneksi = mysqli_connect("192.168.0.50", "inventoryusr", "sZh8QKcKCfWB/Lcf", "inventory");
 
 if (mysqli_connect_errno()) {
 	echo "Koneksi gagal".mysqli_connect_error();
